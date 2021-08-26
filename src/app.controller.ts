29de5b1824c07e08fc989f09c8f22a6a0c143d5b
Elon Aseneka <elonsantos63@gmail.com>
2021-08-27 02:23:01 +0300

    feat(core): add login endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { LoginDetails } from './app.entity';
+import { JwtAuthGuard } from './auth/auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly authService: AuthService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // End point to authenticate a user
+  @Post('auth/login')
+  login(@Body() logins: LoginDetails): any {
+    return this.authService.validateUser(logins);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('auth/confirm')
+  confirmAuth(): any {
+    return {
+      message: 'OK'
+    }
   }
 }
