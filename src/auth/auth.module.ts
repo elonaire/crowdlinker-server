381cfe526f0a143014d68bc58c9b9d92f1b27314
Elon Aseneka <elonsantos63@gmail.com>
2021-08-27 02:05:18 +0300

    feat(core): configure auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { usersProviders } from 'src/users/user.providers';
+import { UsersService } from 'src/users/users.service';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+  imports: [JwtModule.register({
+    secret: `${process.env.SECRET}`,
+    signOptions: { expiresIn: '1h' },
+  })],
+  providers: [AuthService, UsersService, ...usersProviders, JwtStrategy],
+  exports: [AuthService]
 })
 export class AuthModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { usersProviders } from './user.providers';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, ...usersProviders],
+  exports: [UsersService],
 })
 export class UsersModule {}
