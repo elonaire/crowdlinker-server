7842c1f21169bd5ffa14ed4a3f1c472c84a5def2
Elon Aseneka <elonsantos63@gmail.com>
2021-08-27 14:13:24 +0300

    blog auth

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,20 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { BlogPostDto } from './blog.entity';
-import { BlogService } from './blog.service';
+import {Body, Controller, Get, Post, UseGuards} from '@nestjs/common';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/auth.guard';
+import {BlogPostDto} from './blog.entity';
+import {BlogService} from './blog.service';
 
 @Controller('blog')
 export class BlogController {
-    constructor(
-        private readonly blogService: BlogService,
-    ) { }
+  constructor(private readonly blogService: BlogService) {}
 
-    @Get('')
-    fetchBlogPosts(): Promise<BlogPostDto[]> {
-        return this.blogService.fetchBlogPosts();
-    }
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
+  @Get('')
+  fetchBlogPosts(): Promise<BlogPostDto[]> {
+    return this.blogService.fetchBlogPosts();
+  }
 
-    @Post('')
-    createBlogPost(@Body() post: BlogPostDto): Promise<BlogPostDto> {
-        return this.blogService.createBlogPost(post);
-    }
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('Authorization')
+  @Post('')
+  createBlogPost(@Body() post: BlogPostDto): Promise<BlogPostDto> {
+    return this.blogService.createBlogPost(post);
+  }
 }
